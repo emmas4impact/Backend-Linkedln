27c220ee01b0614c67de7bdb6024e5dcdb88f4f1
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-07-21 14:50:28 +0300

    Update index.js

--- src/services/posts/index.js
+++ src/services/posts/index.js
@@ -1,12 +1,22 @@
 const express = require("express")
-
-const PostSchema = require("./schema")
-
+const multer = require("multer")
+//const PostSchema = require("./schema")
+const postModel = require("./schema")
+const path =require("path")
+const fs =require("fs-extra")
 const postRouter = express.Router()
+
+const port = process.env.PORT
+const imagePath = path.join(__dirname, "../../../public/images/post");
+console.log(imagePath)
+const upload = multer({});
+
+
+
 
 postRouter.get("/", async (req, res, next) => {
   try {
-    const post = await PostSchema.find(req.query).populate("profiles")
+    const post = await postModel.find(req.query).populate("profiles")
     res.send(post)
   } catch (error) {
     next(error)
@@ -16,7 +26,7 @@ postRouter.get("/", async (req, res, next) => {
 postRouter.get("/:id", async (req, res, next) => {
   try {
     const id = req.params.id
-    const post = await PostSchema.findById(id)
+    const post = await postModel.findById(id)
     if (post) {
       res.send(post)
     } else {
@@ -34,7 +44,7 @@ postRouter.post("/",
  async (req, res, next) => {
   try {
    
-    const newpost = new PostSchema(req.body)
+    const newpost = new postModel(req.body)
      const { _id } = await newpost.save()
      res.status(201).send(_id)
      
@@ -46,7 +56,7 @@ postRouter.post("/",
  
 postRouter.put("/:id", async (req, res, next) => {
   try {
-    const editPost = await PostSchema.findByIdAndUpdate(req.params.id, req.body)
+    const editPost = await postModel.findByIdAndUpdate(req.params.id, req.body)
    
     if (editPost) {
       res.send("post updated")
@@ -62,7 +72,7 @@ postRouter.put("/:id", async (req, res, next) => {
 
 postRouter.delete("/:id", async (req, res, next) => {
   try {
-    const deletePost = await PostSchema.findByIdAndDelete(req.params.id)
+    const deletePost = await postModel.findByIdAndDelete(req.params.id)
     if (deletePost) {
       res.send("Deleted")
     } else {
@@ -74,5 +84,23 @@ postRouter.delete("/:id", async (req, res, next) => {
     next(error)
   }
 })
+
+postRouter.post("/:id/upload", upload.single("post"), async (req, res, next) => {
+    try {
+      await fs.writeFile(path.join(imagePath, `${req.params.id}.png`), req.file.buffer)
+      req.body = {
+        image: `http://127.0.0.1:${port}/images/post/${req.params.id}.png`
+      }
+      
+      const post =await postModel.findByIdAndUpdate(req.params.id, req.body)
+      if(post){
+          res.send("image uploaded")
+      }
+      
+    } catch (error) {
+      next(error)
+    }
+  })
+
 
 module.exports = postRouter
